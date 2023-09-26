5aee788c17d180458ead604d5befb377920b2530
이홍준 <dlghdwns0219@gmail.com>
2023-09-26 13:23:47 +0900

    이홍준_ADD_무작위스폰
    맵밖에서 생성된 후 플레이어를 따라옴

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -7,7 +7,7 @@ public class Enemy : MonoBehaviour
     public bool isLive = true;
 
     private float _speed = 2;
-    private Rigidbody2D _target;
+    public Rigidbody2D target;
 
     Rigidbody2D rb;
     SpriteRenderer spr;
@@ -20,7 +20,7 @@ public class Enemy : MonoBehaviour
     {
         if (!isLive) return;
 
-        Vector2 dir = _target.position - rb.position; 
+        Vector2 dir = target.position - rb.position; 
         Vector2 nextdir = dir.normalized * _speed * Time.fixedDeltaTime; 
         rb.MovePosition(rb.position + nextdir); 
         rb.velocity = Vector2.zero;    
@@ -29,10 +29,10 @@ public class Enemy : MonoBehaviour
     {
         if (!isLive) return;
 
-        spr.flipX = _target.position.x < rb.position.x;
+        spr.flipX = target.position.x < rb.position.x;
     }
     private void OnEnable()
     {
-        _target = GameManager.instance.player.GetComponent<Rigidbody2D>();
+        target = GameManager.instance.player.GetComponent<Rigidbody2D>();
     }
 }
--- Assets/Scripts/Enemy/Spawner.cs
+++ Assets/Scripts/Enemy/Spawner.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public Transform[] spawnPoint;
+
+    float timer;
+    private void Awake()
+    {
+        spawnPoint = GetComponentsInChildren<Transform>();
+    }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        timer += Time.deltaTime;
+
+        if (timer > 0.5f)
         {
-            GameManager.instance.mob.GetEnemy(0);
+            Spawn();
+            timer = 0f;
         }
+    }
+    public void Spawn()
+    {
+        GameObject enemy = GameManager.instance.mob.GetEnemy(Random.Range(0, 2));
+        enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;     // 위에서 GetComponentsInChildren 로 받아왔기 때문에 0번은 자기자신이라 1부터 시작
     }
 }
