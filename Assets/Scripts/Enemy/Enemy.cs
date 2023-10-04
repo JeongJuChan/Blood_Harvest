03cb8d319de3a636b156857367b527056635224d
juchan <jjc6252498@gmail.com>
2023-10-04 15:37:18 +0900

    [Update]폴더 정리, 불필요한 코드 약간 정리

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEngine.Rendering.VirtualTexturing.Debugging;
 
 public class Enemy : MonoBehaviour, IDamagable
 {
--- Assets/Scripts/Weapons/Base/IUpgrade.cs
+++
@@ -1,4 +0,0 @@
-﻿public interface IUpgrade
-{
-    void Upgrade(float value);
-}
